70e222caaf1f938742d23a2368ac6cc621121dee
mohamedmostafamasarat <m.mostafa@masarat-misr.com>
2021-06-16 22:31:48 +0200

    loading

--- resources/js/components/Secretary/NewTransaction/NewCompanyForm.vue
+++ resources/js/components/Secretary/NewTransaction/NewCompanyForm.vue
@@ -147,7 +147,7 @@
                                                 <v-autocomplete v-model="institution.legal_entity" :rules="required" outlined item-text="name" item-value="value" :items="legal_entityOptions" :label="$t('legal_entity')" required />
                                             </v-col>
                                             <v-col cols="12" sm="6" md="2">
-                                                <v-text-field v-model="institution.company_nationality" :rules="required" outlined autocomplete="company_nationality" :label="$t('company_nationality')" required />
+                                                <v-autocomplete v-model="institution.company_nationality" outlined :rules="required" :items="['سعوديه', 'أجنبيه', 'مختلطه']" autocomplete="company_nationality" :label="$t('company_nationality')" required></v-autocomplete>
                                             </v-col>
                                             <v-col cols="12" sm="6" md="2">
                                                 <v-text-field v-model="institution.company_period" :rules="required" outlined autocomplete="company_period" :label="$t('company_period')" required />
